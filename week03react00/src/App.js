96dbf68496b3fb7164b7eabacaf5e23cdf7ce94d
Nam-Dang13581234 <nathandang73@gmail.com>
2020-08-14 11:31:48 +1000

    Init commit

---
+++ week03react00/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      username: '',
+      showing: 'askName'
+    };
+  }
+
+  render() {
+    let page;
+    if(this.state.showing === 'askName'){
+      page = (
+        <div>
+        <div>What is your name?</div>
+        <input type='text' name='username' value={this.state.username} onChange={(e) => this.setState({username: e.target.value})}></input>
+        <button onClick={(e) => this.setState({showing: ''})}>Click here</button>
+      </div>
+      );
+    } else{
+      page = (
+      <div>
+        Welcome {this.state.username}
+      </div>
+      );
+    }
+    return page;
+  }
+}
+
+export default App;
---
+++ week03react00/src/Converter.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+function Converter(){
+    const [type, setType] = useState(0);
+    const [value, setValue] = useState(0);
+    const [result, setResult] = useState(0);
+
+    return (
+        <div>
+            <p>
+                
+            </p>
+        </div>
+    );
+}
+
+
+export default Converter;
