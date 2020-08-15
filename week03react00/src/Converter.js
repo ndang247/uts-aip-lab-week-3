25752e5953d1370f63db2bc805862a98ac8982cf
Nam-Dang13581234 <nathandang73@gmail.com>
2020-08-15 17:01:59 +1000

    Reset output

--- week03react00/src/Converter.js
+++ week03react00/src/Converter.js
@@ -1,54 +1,54 @@
 import React from 'react';
 
-class Selector extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            type: ''
-        }
-    }
-    render() {
-        return (
-        <div>
-        <h1>Imperial to Metric Converter</h1>
-        <p>
-            <button onClick={() => this.setState({type: 'Temperature'})}>
-                Temperature
-            </button>
-            <button onClick={() => this.setState({type: 'Distance'})}>
-                Distance
-            </button>
-            <p/>
-            <Converter type={this.state.type}/>
-        </p>
-        </div>
-        );
-    }
-}
-
 class Converter extends React.Component {
     constructor(props){
       super(props);
       this.state = {
-        value: '',
-        result: ''
+          type: '',
+          value: '',
+          result: ''
       };
       this.calculate = this.calculate.bind(this);
     }
+
+    resetResult() {
+        this.setState({result: ''});
+    }
   
     render() {
-      let page;
-      let input = (
-          <div>
-              <input type='text' value={this.state.value} onChange={(e) => this.setState({value: e.target.value})}></input>
-              <p></p>
-              <button onClick={this.calculate}>Convert</button>
-          </div>
-          );
+        let page;
+        const selector = (
+            <div>
+                <h1>Imperial to Metric Converter</h1>
+                <p>
+                    <button onClick={ () => {
+                        this.setState({type: 'Temperature'});
+                        this.resetResult();
+                        }}>
+                        Temperature
+                    </button>
+                    <button onClick={ () => {
+                        this.setState({type: 'Distance'});
+                        this.resetResult();
+                        }}>
+                        Distance
+                    </button>
+                    <p/>
+                </p>
+            </div>
+        );
+        const input = (
+            <div>
+                <input type='text' value={this.state.value} onChange={(e) => this.setState({value: e.target.value})}></input>
+                <p></p>
+                <button onClick={this.calculate}>Convert</button>
+            </div>
+        );
 
-      if (this.props.type === 'Temperature') {
+      if (this.state.type === 'Temperature') {
         page = (
         <div>
+            {selector}
             <label>Fahrenheit: </label>
             {input}
             <p>Celsius: {this.state.result}</p>
@@ -57,6 +57,7 @@ class Converter extends React.Component {
       } else {
         page = (
         <div>
+            {selector}
             <label>Miles: </label>
             {input}
             <p>Kilometers: {this.state.result}</p>
@@ -67,7 +68,7 @@ class Converter extends React.Component {
     }
 
     calculate() {
-        if (this.props.type === 'Temperature') {
+        if (this.state.type === 'Temperature') {
             let temperatureResult = (this.state.value - 32) * 5/9;
             this.setState({result: temperatureResult});   
         } else {
@@ -78,4 +79,4 @@ class Converter extends React.Component {
     }
   }
 
-  export default Selector;  
+  export default Converter; 
--- week03react00/src/index.js
+++ week03react00/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Converter from './Converter';
+import Selector from './Converter';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Converter />
+    <Selector />
   </React.StrictMode>,
   document.getElementById('root')
 );
